5e4a6e61c0f794f98495bc8d601f7160b41726c8
Luanoliveira14 <aluno@etec.sp.gov.br>
2023-11-14 20:44:25 -0300

    Aula1411 Fundamentos de DOM HTML

--- public/js/action.js
+++ public/js/action.js
@@ -1,14 +1,37 @@
-function somaNumero(n1, n2, op){
-    let resultado=0;
-    if(op==="c"){
-        resultado=(n1+n2)
-        console.table(resultado)
-        return
-    }
+function calculaNumeros()
+{
+    let n1 = parseInt(document.getElementById("usuario").value)
+    let n2 = parseInt(document.getElementById("senha").value)
+    let op = (document.getElementById("operation").value)
 
-    if(op==="a"){
-        resultado=(n1 - n2)
-        alert(resultado)
-        return
+    if(op === "+")
+    {
+        resultado = (n1 + n2)
+        alert("soma: "+n1+" "+op+" "+n2+" = "+resultado)
+    }
+    else if (op === "-")
+    {
+        resultado = (n1 - n2)
+        alert("subtração: "+n1+" "+op+" "+n2+" = "+resultado)
+    }
+    else if (op === "*")
+    {
+        resultado = (n1 * n2)
+        alert("multipicação: "+n1+" "+op+" "+n2+" = "+resultado)
+    }
+    else if (op === "/")
+    {
+        resultado = (n1 / n2)
+        alert("divisão: "+n1+" "+op+" "+n2+" = "+resultado)
+    }
+    else if (op === "sqrt")
+    {
+        resultado = Math.sqrt(n1 + n2)
+        alert("Raiz : "+n1+" "+op+" "+n2+" = "+resultado)
+    }
+    else if (op === "sqrt")
+    {
+        resultado = Math.sqrt()
+        alert(" : "+n1+" "+op+" "+n2+" = "+resultado)
     }
 }
